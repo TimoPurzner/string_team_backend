6c461de12b7bc6f79cef3eb149b935ee75072081
Purble Heart <neptune@planeptune.asdf>
2019-11-23 00:31:13 +0100

    added cors for corssite api usage

--- api.py
+++ api.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+CORS(app)
+
 
 db = SQLAlchemy(app)
 
