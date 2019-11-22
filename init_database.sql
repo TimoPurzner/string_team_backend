e8a799a0c4cfa13a1a1c20ba8c4e7ea27817f964
Fabian Eckstein <diadochokinetic@gmail.com>
2019-11-22 21:39:38 +0100

    update init_database.sql

--- init_database.sql
+++ init_database.sql
@@ -11,3 +11,15 @@ INSERT INTO user (id, name, psid, group_name) VALUES (0, "Fabian", 1337, "defaul
 
 
 SELECT * FROM user;
+
+CREATE TABLE groupname(
+	id int PRIMARY KEY,
+	group_name char(50),
+	valid_psid char(256)
+);
+
+
+INSERT INTO groupname (id, group_name, valid_psid) VALUES (0, "default", "{'psids':[1337]}");
+
+
+SELECT * FROM groupname;
